b3bc6bc98e5b61c3b04f325775a2a29d0a2bf317
Iago Reis Homsi <iagohomsi@gmail.com>
2024-05-19 20:07:18 -0300

    Slider refeito e ajustes feitos
    Slider refeito com alguns ajustes de imagem, infelizmente a interação do "cursorGrab" não está funcionando.
    
    Nav-bar reajustada para mobile, tinha parado de funcionar depois da transição dos arquivos.

---
+++ contatos/src/main/webapp/swiper.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".swiper", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    keyboard: true,
+    scrollbar: false,
+    autoplay: false,
+    cursorGrab: true,
+});
